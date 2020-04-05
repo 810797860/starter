f21a9bd0211b1a7287567f81f379406ea4fcec5b
810797860 <810797860@qq.com>
2020-04-05 19:50:47 +0800

    新电脑配环境+按钮动态显示(form表单的)

--- starter-admin/starter-admin-controller/src/main/resources/static/js/system/form/tabulation.js
+++ starter-admin/starter-admin-controller/src/main/resources/static/js/system/form/tabulation.js
@@ -194,6 +194,30 @@ layui.use(['laypage', 'layer', 'table', 'element'], function(){
         return switchStr;
     }
 
+    //单击行勾选checkbox事件
+    $(document).on("click", ".layui-table-body table.layui-table tbody tr", function () {
+        var index = $(this).attr('data-index');
+        var tableBox = $(this).parents('.layui-table-box');
+        //存在固定列
+        if (tableBox.find(".layui-table-fixed.layui-table-fixed-l").length > 0) {
+            tableDiv = tableBox.find(".layui-table-fixed.layui-table-fixed-l");
+        } else {
+            tableDiv = tableBox.find(".layui-table-body.layui-table-main");
+        }
+        var CheckLength = tableDiv.find("tr[data-index=" + index + "]").find(
+            "td div.layui-form-checked").length;
+
+        var checkCell = tableDiv.find("tr[data-index=" + index + "]").find(
+            "td div.laytable-cell-checkbox div.layui-form-checkbox I");
+        if (checkCell.length > 0) {
+            checkCell.click();
+        }
+    });
+
+    $(document).on("click", "td div.laytable-cell-checkbox div.layui-form-checkbox", function (e) {
+        e.stopPropagation();
+    });
+
     /**
      * 给子页面定义函数
      */
